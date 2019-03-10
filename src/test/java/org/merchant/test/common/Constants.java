0dd617b71409ca58477dda5b68c2c6367dca8570
vasanth-soundararajan <vasanth.soundararajan@gmail.com>
2019-03-10 13:28:01 -0400

    Make change to server URL

--- src/test/java/org/merchant/test/common/Constants.java
+++ src/test/java/org/merchant/test/common/Constants.java
@@ -2,6 +2,6 @@ package org.merchant.test.common;
 
 public class Constants {
 
-    public static final String BASE_URL = "http://ec2-54-174-213-136.compute-1.amazonaws.com:8080/admin";
+    public static final String BASE_URL = "http://ec2-54-84-52-184.compute-1.amazonaws.com:8080/admin";
 
 }
